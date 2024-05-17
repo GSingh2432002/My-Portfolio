91f316e7ba2034588adb0fbe28fab6086997a037
GSingh2432002 <gsingh332211@gmail.com>
2024-05-17 23:21:33 +0530

    Update

--- Main_Portfolio/src/Components/About.jsx
+++ Main_Portfolio/src/Components/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
                         <br />
                         <br />Feel free to connect with me, links are in the footer section.
                     </p>
-                    <a href="https://drive.google.com/file/d/1tgPFZxRbf2vZojvK75HJ-wu00xPm6_Ue/view?usp=sharing" download className="resume-btn">
+                    <a href="https://drive.google.com/file/d/1qE_EBQJEzpWnQ6G1aWR-nv12Wo0Piue2/view?usp=sharing" download className="resume-btn">
                         <button>
                             Download Resume <FaFileDownload></FaFileDownload>
                         </button>
